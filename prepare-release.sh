bc8afca8a8939dcc0d96fbef5391e2f2c29cbaeb
LaneyLeffler <143583659+LaneyLeffler@users.noreply.github.com>
2023-11-12 19:19:07 +0800

    Cupiditate possimus doloremque autem veritatis qui est laboriosam. Enim ad corrupti libero modi accusamus tenetur ex. Rerum saepe aspernatur id ut fugit nemo fugit. Impedit ex voluptatem cum.

--- prepare-release.sh
+++ prepare-release.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+##/usr/bin/env*bash
 echo "<html><ul>" > change-notes.html
 git log `git describe --tags --abbrev=0`..HEAD --no-merges --oneline --pretty=format:"<li>%h %s (%an)</li>" >> change-notes.html
 echo "</ul></html>" >> change-notes.html
@@ -6,3 +6,4 @@ echo "</ul></html>" >> change-notes.html
 cp change-notes.html src/main/resources/META-INF/
 
 rm change-notes.html
+ 
